2cb2401eca0d1e1e293c53bd942e3c29509f73b2
Anii <79158719+lelouch-of-the-code@users.noreply.github.com>
2022-12-04 15:43:57 +0530

    day4

---
+++ day4/A.py
@@ -0,0 +1,7 @@
+f=open("input.txt","r")
+res=0
+pres=0
+for line in f:
+	r=sorted([[int(i) for i in s.split('-')] for s in line.split(',')])
+	res+=int(r[0][1]>=r[1][1] or r[0][0]==r[1][0])
+print(res)
